064ff03d3f217a236a852a15cf8639f39e0d2268
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-12-14 22:13:01 -0300

    Add global CSS and create a shape using CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import styles from '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
@@ -10,13 +11,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
+          <div className={styles.shape}></div>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
