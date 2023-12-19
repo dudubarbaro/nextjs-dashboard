55d067508d822d8ca67187c628d82ece15df098e
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-12-19 00:45:46 -0300

    Add useFormState hook to EditInvoiceForm

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
+import { useFormState } from '@/app/hooks/use-form-state';
 
 export default function EditInvoiceForm({
   invoice,
